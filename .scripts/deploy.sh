dd814731850759bdc29e47ae20ddac5eae31652e
Jeremy <jeremykenedy@gmail.com>
2022-12-24 22:51:05 -0800

    update deployment

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,14 +3,14 @@ set -e
 
 echo "Deployment started ..."
 
-
+cd ../
 
 # # Enter maintenance mode or return true
 # # if already is in maintenance mode
-# (php artisan down) || true
+(php artisan down) || true
 
 # # Pull the latest version of the app
-# git pull origin production
+git pull origin production
 
 # # Install composer dependencies
 # composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
@@ -27,7 +27,7 @@ echo "Deployment started ..."
 # # Run database migrations
 # php artisan migrate --force
 
-touch hello.txt
+
 
 # # Exit maintenance mode
 # php artisan up
