d5b0fcc0c9f5891cae7aad684618ead84ed561be
Jeremy <jeremykenedy@gmail.com>
2022-12-28 17:21:07 -0800

    update deployment script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
 
-echo "Deployment started ..."
+echo "Deployment started ... ..."
 
 # # Enter maintenance mode or return true
 # # if already is in maintenance mode
 php artisan down
 
-# git reset --hard
+git reset --hard
 
 # echo 'Hellooo'
 
 
 # # Pull the latest version of the app
-# git pull origin production
+git pull origin production
 
 # # Install composer dependencies
-# composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
 # # Clear the old cache
 # php artisan clear-compiled
@@ -35,6 +35,6 @@ php artisan down
 # # Exit maintenance mode
 # php artisan up
 
-echo "Deployment finished!"
+echo "Deployment finished Successfully!"
 
 
