a7d6fb6724791c00fa14b9a22e4eefaf8d297dd2
Jeremy <jeremykenedy@gmail.com>
2023-01-05 00:10:48 -0800

    add logger backend, https force, and update deployment script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,20 +1,60 @@
 #!/bin/bash
-set -e
 
-echo "Deployment started ... ..."
-(php artisan down) || true
-export NODE_OPTIONS=--max-old-space-size=8192
-git fetch --all
-git checkout released
-git reset --hard
-git pull origin released
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
-php artisan clear-compiled
-php artisan config:cache
-php artisan optimize
-npm install
-npm run build
-php artisan migrate --force
-php artisan db:seed
-php artisan up
-echo "Deployment finished Successfully!"
+BRANCH=${1:-$(git branch --show-current)}
+
+deployStart() {
+    set -e
+    echo "Deployment started ... ..."
+    (php artisan down) || true
+    export NODE_OPTIONS=--max-old-space-size=8192
+}
+
+deployGit() {
+    git fetch --all
+    git fetch --tags
+    git checkout $BRANCH
+    # git reset --hard
+    git pull origin $BRANCH
+}
+
+deployComposer() {
+    composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+    composer update --no-dev --no-interaction --prefer-dist --optimize-autoloader
+}
+
+deployArtisanCommands() {
+    php artisan clear-compiled
+    php artisan config:cache
+    php artisan optimize
+}
+
+deployMigrationsAndSeeds() {
+    php artisan migrate --force
+    php artisan db:seed
+}
+
+deployFrontEnd() {
+    npm install
+    npm update
+    npm run clean
+    npm run lint
+    npm run optimize
+    npm run build
+}
+
+deployFinish() {
+    php artisan up
+    echo "Deployment finished Successfully!"
+}
+
+deploy() {
+    deployStart
+    deployGit
+    deployComposer
+    deployArtisanCommands
+    deployMigrationsAndSeeds
+    deployFrontEnd
+    deployFinish
+}
+
+deploy
