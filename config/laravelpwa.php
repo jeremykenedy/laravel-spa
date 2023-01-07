ff8a23fabc04ba85934793b3212d78db0fdba24d
Jeremy <jeremykenedy@gmail.com>
2023-01-07 11:43:57 -0800

    build process for PWA

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -3,8 +3,8 @@
 return [
     'name' => 'LaravelPWA',
     'manifest' => [
-        'name' => env('APP_NAME', 'My PWA App'),
-        'short_name' => 'PWA',
+        'name' => env('APP_NAME', 'Laravel Auth Spa'),
+        'short_name' => 'LAS',
         'start_url' => '/',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
