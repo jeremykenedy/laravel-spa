d719d358ef65170fe7c79f4698d70ef7c7092073
Jeremy <jeremykenedy@gmail.com>
2023-01-07 17:18:34 -0800

    update serviceworker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,6 +1,6 @@
 var staticCacheName = "pwa-v" + new Date().getTime();
 var filesToCache = [
-    '/offline',
+    '/build',
     '/css/app.css',
     '/js/app.js',
     '/images/icons/icon-72x72.png',
