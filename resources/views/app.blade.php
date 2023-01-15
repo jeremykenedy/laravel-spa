fbfc44b97cd2cd39aa7b57a1ee67dbe4b0abbe1a
Jeremy <jeremykenedy@gmail.com>
2023-01-15 13:28:47 -0800

    build process

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,7 +20,7 @@
         <meta name="author" content="{{ config('settings.author') }}">
         <link rel="icon" href="{{ config('app.url') }}/favicon.ico">
         <link rel="apple-touch-icon" href="{{ config('app.url') }}/apple-touch-icon.png" sizes="180x180">
-        <link rel="mask-icon" href="/favicon-32x32.png" color="#FFFFFF">
+        <link rel="mask-icon" href="{{ config('app.url') }}/favicon-32x32.png" color="#FFFFFF">
         <meta name="theme-color" content="#ffffff">
         <script>
             const GA_ENABLED="{{ $ga_enabled }}"
@@ -36,13 +36,13 @@
             const VULTR_REFERRAL_LINK="{{ config('settings.vultrReferralLink') }}"
         </script>
         @vite(['resources/css/app.css'])
-        @laravelPWA
+        {{-- @laravelPWA --}}
     </head>
     <body class="bg-gray-50 h-screen antialiased leading-none font-sans overflow-x-hidden overflow-y-auto">
         <div id="app"  v-cloak></div>
         @vite(['resources/js/app.js'])
         @if(config('settings.enableKonamiAsteroids'))
-            <script type="text/javascript" src="/js/s-code.min.js"></script>
+            <script type="text/javascript" src="{{ config('app.url') }}/js/s-code.min.js"></script>
         @endif
     </body>
 </html>
