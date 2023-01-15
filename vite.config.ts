cba7fac06e85ce527a22583be2e41d98c41bc8e8
Jeremy <jeremykenedy@gmail.com>
2023-01-15 13:37:58 -0800

    build process

--- vite.config.ts
+++ vite.config.ts
@@ -109,54 +109,54 @@ export default ({ mode }) => {
       reportCompressedSize: true,
       chunkSizeWarningLimit: 1600,
       manifest: true,
-      sourcemap: true,
-      // rollupOptions: {
-      //   output: {
-      //     manualChunks(id, { getModuleInfo }) {
-      //       const match = /.*\.strings\.(\w+)\.js/.exec(id);
-      //       if (match) {
-      //         const language = match[1]; // e.g. "en"
-      //         const dependentEntryPoints = [];
+      sourcemap: process.env.VITE_APP_ENV == 'local' ? true : false,
+      rollupOptions: {
+        output: {
+          manualChunks(id, { getModuleInfo }) {
+            const match = /.*\.strings\.(\w+)\.js/.exec(id);
+            if (match) {
+              const language = match[1]; // e.g. "en"
+              const dependentEntryPoints = [];
 
-      //         // we use a Set here so we handle each module at most once. This
-      //         // prevents infinite loops in case of circular dependencies
-      //         const idsToHandle = new Set(getModuleInfo(id).dynamicImporters);
+              // we use a Set here so we handle each module at most once. This
+              // prevents infinite loops in case of circular dependencies
+              const idsToHandle = new Set(getModuleInfo(id).dynamicImporters);
 
-      //         for (const moduleId of idsToHandle) {
-      //           const { isEntry, dynamicImporters, importers } =
-      //             getModuleInfo(moduleId);
-      //           if (isEntry || dynamicImporters.length > 0)
-      //             dependentEntryPoints.push(moduleId);
+              for (const moduleId of idsToHandle) {
+                const { isEntry, dynamicImporters, importers } =
+                  getModuleInfo(moduleId);
+                if (isEntry || dynamicImporters.length > 0)
+                  dependentEntryPoints.push(moduleId);
 
-      //           // The Set iterator is intelligent enough to iterate over elements that
-      //           // are added during iteration
-      //           for (const importerId of importers) idsToHandle.add(importerId);
-      //         }
+                // The Set iterator is intelligent enough to iterate over elements that
+                // are added during iteration
+                for (const importerId of importers) idsToHandle.add(importerId);
+              }
 
-      //         // If there is a unique entry, we put it into a chunk based on the entry name
-      //         if (dependentEntryPoints.length === 1) {
-      //           return `${
-      //             dependentEntryPoints[0].split('/').slice(-1)[0].split('.')[0]
-      //           }.strings.${language}`;
-      //         }
-      //         // For multiple entries, we put it into a "shared" chunk
-      //         if (dependentEntryPoints.length > 1) {
-      //           return `shared.strings.${language}`;
-      //         }
-      //       }
-      //     },
-      //     globals: {
-      //       vue: 'Vue',
-      //     },
-      //   },
-      //   external: ['Vue'],
-      // },
-      // modulePreload: {
-      //   polyfill: true,
-      // },
-      // commonjsOptions: {
-      //   include: [/node_modules/],
-      // },
+              // If there is a unique entry, we put it into a chunk based on the entry name
+              if (dependentEntryPoints.length === 1) {
+                return `${
+                  dependentEntryPoints[0].split('/').slice(-1)[0].split('.')[0]
+                }.strings.${language}`;
+              }
+              // For multiple entries, we put it into a "shared" chunk
+              if (dependentEntryPoints.length > 1) {
+                return `shared.strings.${language}`;
+              }
+            }
+          },
+          globals: {
+            vue: 'Vue',
+          },
+        },
+        external: ['Vue'],
+      },
+      modulePreload: {
+        polyfill: true,
+      },
+      commonjsOptions: {
+        include: [/node_modules/],
+      },
     },
     plugins: [
       laravel({
@@ -277,24 +277,24 @@ export default ({ mode }) => {
           ],
         },
       }),
-      // legacy({
-      //   targets: ['defaults', 'not IE 11'],
-      //   polyfills: true,
-      // }),
-      // splitVendorChunkPlugin(),
-      // chunkSplitPlugin(),
-      // Pages({
-      //   onRoutesGenerated: async (routes) => {
-      //     generateSitemap({
-      //       hostname: process.env.VITE_APP_NAME,
-      //       routes: [...routes],
-      //       readable: true,
-      //       exclude: ['/admin'],
-      //       allowRobots: false,
-      //       filename: 'sitemap.xml',
-      //     });
-      //   },
-      // }),
+      legacy({
+        targets: ['defaults', 'not IE 11'],
+        polyfills: true,
+      }),
+      splitVendorChunkPlugin(),
+      chunkSplitPlugin(),
+      Pages({
+        onRoutesGenerated: async (routes) => {
+          generateSitemap({
+            hostname: process.env.VITE_APP_NAME,
+            routes: [...routes],
+            readable: true,
+            exclude: ['/admin'],
+            allowRobots: false,
+            filename: 'sitemap.xml',
+          });
+        },
+      }),
       // VitePWA({
       //   srcDir: 'public',
       //   filename: 'sw.ts',
@@ -360,16 +360,16 @@ export default ({ mode }) => {
       //     buttonText: 'refresh',
       //   },
       // }),
-      // manifestSRI(),
-      // createHtmlPlugin({
-      //   minify: true,
-      //   entry: 'resources/js/app.js',
-      // }),
-      // ViteMinifyPlugin({
-      //   minifyCSS: true,
-      //   removeComments: true,
-      // }),
-      // viteCommonjs(),
+      manifestSRI(),
+      createHtmlPlugin({
+        minify: true,
+        entry: 'resources/js/app.js',
+      }),
+      ViteMinifyPlugin({
+        minifyCSS: true,
+        removeComments: true,
+      }),
+      viteCommonjs(),
       // basicSsl(),
       // SentryPlugin,
       // InspectPlugin,
